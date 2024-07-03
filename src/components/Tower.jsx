af9b439cfbd2b3962ba4469d9d34c861605573e0
Greg Anderson <greg.anderson.cs@gmail.com>
2024-07-02 19:23:47 -0600

    Remove reference to inline svg

--- src/components/Tower.jsx
+++ src/components/Tower.jsx
@@ -1,18 +1,6 @@
 import Disc from "./Disc";
 import styles from "./Tower.module.css";
 
-const baseColor = "blue";
-const towerColor = "yellow";
-
-const towerSVG = `
-<svg width="90%" height="100%" xmlns="http://www.w3.org/2000/svg">
-  <rect width="100%" height="10%" x="0%" y="90%" fill="${baseColor}" />
-  <rect width="10%" height="90%" x="45%" y="0%" fill="${towerColor}" />
-</svg>
-`;
-
-const towerImage = `data:image/svg+xml,${encodeURIComponent(towerSVG)}`;
-
 export default function Tower({ discs }) {
 
 
